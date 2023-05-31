57063ff2fdc951175818a6ffbf6632d7b6babccb
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2023-05-31 08:07:27 +0200

    Delete CreateTaskDto.cs

--- WebApka/CreateTaskDto.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApka.Models;
-
-public class CreateTaskDto
-{
-    public string Title {get; set;}
-    public string Content {get; set;}
-    public DateTime EndTime { get; set; }
-    public int UserId { get; set; }
-}
