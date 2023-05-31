cac705aadc2dd085343571b0be8d92dbb4c1f341
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2023-05-31 08:06:54 +0200

    Delete UpdateTaskDto.cs

--- WebApka/UpdateTaskDto.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApka.Models;
-
-public class UpdateTaskDto
-{
-    public string Title {get; set;}
-    public string Content {get; set;}
-    public DateTime EndTime { get; set; }
-    public int UserId { get; set; }
-}
