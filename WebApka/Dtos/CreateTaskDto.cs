da1b914bdd16f290c7786c00a8980d117c8be070
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2023-06-05 07:52:07 +0200

    Add files via upload

--- WebApka/Dtos/CreateTaskDto.cs
+++ WebApka/Dtos/CreateTaskDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApka.Models;
 
 public class CreateTaskDto
-{
+{   
+    [Required(ErrorMessage = "Please enter a title")]
     public string Title {get; set;}
+
+    [Required(ErrorMessage = "Please enter a content")]
     public string Content {get; set;}
+
+    [Required(ErrorMessage = "Please enter a end time")]
     public DateTime EndTime { get; set; }
     public int UserId { get; set; }
 }
